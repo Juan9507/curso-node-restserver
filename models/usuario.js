fe35101828015abf94393f8753473846ad31cb8a
juan9507 <juandavidnaranjo75@gmail.com>
2022-12-13 15:12:05 -0500

    Se agrego login con google identity - google sign in

--- models/usuario.js
+++ models/usuario.js
@@ -20,7 +20,9 @@ const UsuariosSchema = Schema({
     },
     rol: {
         type: String,
-        required: [true, 'El rol es obligatorio'],
+        required: true,
+        default: 'USER_ROLE',
+        emun: ['ADMIN_ROLE', 'USER_ROLE']
         // enum: ['ADMIN_ROLE', 'USER_ROLE']
     },
     estado: {
